1cf0f140f1cd29669731b0db179c0de70ba3d427
PDDaniel <daniel@proper-dog.com>
2016-01-05 10:49:31 +0100

    Save

---
+++ DecisionMaking-Methods/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DecisionMaking_Methods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int id = -1)
+        {
+            return View(id);
+        }
+
+        [HttpPost]
+        public ActionResult GetNumbers(int number1, int number2)
+        {
+            int result = number1 + number2;
+
+            return RedirectToAction("Index", "Home", new { id = result });
+        }
+    }
+}
---
+++ DecisionMaking-Methods/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DecisionMaking_Methods.MvcApplication" Language="C#" %>
